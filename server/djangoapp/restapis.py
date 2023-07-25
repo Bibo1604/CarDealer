25db97a31d5adf2fb321a938c96625e11822c699
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-07-24 18:38:08 -0600

    Add dynamic page

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,6 +2,9 @@ import requests
 import json
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features,SentimentOptions
 
 
 def get_request(url, **kwargs):
@@ -33,6 +36,16 @@ def get_request(url, **kwargs):
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
+def post_request(url, json_payload, **kwargs):
+    print(kwargs)
+    print("POST to {} ".format(url))
+    print(json_payload)
+
+    response = requests.post(url, params=kwargs, json=json_payload)
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 
 def get_dealers_from_cf(url, **kwargs):
@@ -57,14 +70,14 @@ def get_dealers_from_cf(url, **kwargs):
     return results
 
 
-def get_dealer_by_id_from_cf(url, dealerId):
+def get_dealer_by_id_from_cf(url, id):
     results = []
-    json_result = get_request(url)
+    json_result = get_request(url, id=id)
     if json_result:
         dealers = json_result
         for dealer in dealers:
-            dealer_doc = dealer["doc"]
-            if dealer_doc["id"] == dealerId:
+            dealer_doc = dealer
+            if dealer_doc["id"] == id:
                 dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
                                    short_name=dealer_doc["short_name"],
@@ -108,7 +121,16 @@ def get_dealer_reviews_from_cf(url, **kwargs):
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
 def analyze_review_sentiments(text):
-    return
-
+    url = "https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/46005b7e-d45a-4c6e-b902-255282eb261e"
+    api_key = "WPWOg1B-rw20MTR6pRPzzUBkHCAmWykMopyhFv31-McK"
+    authenticator = IAMAuthenticator(api_key)
+    natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator)
+    natural_language_understanding.set_service_url(url)
+    response = natural_language_understanding.analyze( text=text+"hello hello hello",features=Features(sentiment=SentimentOptions(targets=[text+"hello hello hello"]))).get_result()
+    label=json.dumps(response, indent=2)
+    label = response['sentiment']['document']['label']
+    
+    
+    return(label)
 
 
